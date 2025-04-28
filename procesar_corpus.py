3ebc2c3553139270c82c95f80fcac50f1f5327b5
avillaq <villafuertequispealex@gmail.com>
2025-04-28 18:11:25 -0500

    carpeta cess_esp

--- procesar_corpus.py
+++ procesar_corpus.py
@@ -1,4 +1,4 @@
 from autocorrect.word_count import count_words, extraer_texto
 
-extraer_texto(ruta_carpeta="corpus", archivo_salida="data/cess_esp_texto.txt")
+extraer_texto(ruta_carpeta="cess_esp_corpus", archivo_salida="data/cess_esp_texto.txt")
 count_words(src_filename="data/cess_esp_texto.txt", lang="es", encd="utf-8", out_filename="data/cess_esp_word_count.json")
